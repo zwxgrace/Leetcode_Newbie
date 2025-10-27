713008dac063d769f233586a105c255b22e73566
Wenxu Zhang <wenxu.zhang@polytechnique.edu>
2025-10-27 03:05:54 +0200

    Add function call and print result for temperatures

---
+++ 739_Daily_Temperatures.py
@@ -0,0 +1,22 @@
+# 739. Daily Temperatures
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        result = [0]*n    # 0 is the answer of not finding a larger datapoint
+        stack = []    # initialize a stack
+
+        for i in range(n):
+            # always compare with the latest added one in stack(stack top)
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                # when the task is satisified, do 2 things: 
+                # pop up the finished index(to keep the stack monotonically increasing); 
+                # calculate the result since the task is done for this index
+                prev_index = stack.pop()
+                result[prev_index]=i-prev_index
+            # when not satisified, just keep the new one in the stack, waiting for being solved
+            stack.append(i)
+        return result
+
+T = [24, 26, 28, 18, 17, 22]
+result_t= dailyTemperatures(T)
+print(result_t)
